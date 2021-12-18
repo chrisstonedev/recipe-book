ca15de61ce27185e9089da990cdc72f8d2314846
Chris Stone <contact@chrisstone.dev>
2021-12-17 22:04:09 -0500

    Adding pages and content and learning about Head, Link, and Router

---
+++ pages/pokemon.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+
+function Pokemon({pokemon}) {
+    return (
+        <>
+            <Head>
+                <title>Pokemon: {pokemon?.name}</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+            </Head>
+            <div>
+                Welcome, {pokemon?.name}!
+                <img src={pokemon?.sprites.front_default} alt={pokemon?.name}/>
+            </div>
+        </>
+    );
+}
+
+export async function getStaticProps() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/charmander');
+    const pokemon = await response.json();
+
+    return {
+        props: {
+            pokemon
+        }
+    }
+}
+
+export default Pokemon
